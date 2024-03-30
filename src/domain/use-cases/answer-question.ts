05143b84c6f1cfb202c6f741f28c31d77c710f64
Henrriky <tectutors.123@gmail.com>
2024-03-30 00:36:20 -0300

    feat: add path aliases and vitest globals to unit tests

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,4 @@
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 import { Answer } from "../entities/Answer"
 import { AnswersRepository } from "../repositories/answers-repository"
 
