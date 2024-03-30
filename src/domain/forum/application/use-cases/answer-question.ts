d0d1f2920c0b809b4a2571bd4959080e5b145eb6
Henrriky <tectutors.123@gmail.com>
2024-03-30 04:45:53 -0300

    refactor: move files to clean architecture folder structure

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,5 +1,5 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Answer } from '../entities/Answer'
+import { Answer } from '@/domain/forum/enterprise/entities/Answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseInput {
