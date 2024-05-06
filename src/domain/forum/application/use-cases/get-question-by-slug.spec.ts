5478e87641975a40507150fe33de9ee1c616344b
Henrriky <tectutors.123@gmail.com>
2024-05-05 23:47:45 -0300

    feat: create get question by slug usecase, unit test and question creation factory

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,30 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { makeQuestion } from 'test/factories/make-question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+
+let inMemoryQuestionRepository: InMemoryQuestionsRepository
+let usecase: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionsRepository()
+    usecase = new GetQuestionBySlugUseCase(inMemoryQuestionRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = makeQuestion({
+      slug: Slug.create('example-question'),
+    })
+
+    await inMemoryQuestionRepository.create(newQuestion)
+
+    const { question } = await usecase.execute({
+      slug: newQuestion.slug.value,
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(newQuestion.title)
+  })
+})
