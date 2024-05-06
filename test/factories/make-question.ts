3cdfecd8f533584cb9e9a1cb7c9b2346195419d5
Henrriky <tectutors.123@gmail.com>
2024-05-05 23:52:56 -0300

    feat: add fakerjs to mock data of unit tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,25 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { faker } from '@faker-js/faker'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/Question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  id?: UniqueEntityID,
+  override: Partial<QuestionProps> = {},
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
