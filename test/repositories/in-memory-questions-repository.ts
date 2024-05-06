568f9c6a3222f1042ec8f76aa0e9e3e2e87d9d67
Henrriky <tectutors.123@gmail.com>
2024-05-06 00:33:48 -0300

    feat: create findById and delete methods in QuestionRepository and InMemRepository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -2,19 +2,31 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
 import { Question } from '@/domain/forum/enterprise/entities/Question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
-  public questions: Question[] = []
+  public items: Question[] = []
 
   async create(question: Question) {
-    this.questions.push(question)
+    this.items.push(question)
   }
 
   async findBySlug(slug: string) {
-    const question = this.questions.find(
-      (question) => question.slug.value === slug,
-    )
+    const question = this.items.find((item) => item.slug.value === slug)
 
     if (!question) return null
 
     return question
+  }
+
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) return null
+
+    return question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
