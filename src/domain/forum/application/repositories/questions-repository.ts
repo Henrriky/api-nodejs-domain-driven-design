38ac335085bd7069c4fbb9e11cc63f7369909b8f
Henrriky <tectutors.123@gmail.com>
2024-06-26 22:23:10 -0300

    feat: crete abstract method to question repository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,11 @@
+import { PaginationParams } from '@/core/repositories/paginations-params'
 import { Question } from '../../enterprise/entities/Question'
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>
+  findBySlug(slug: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   create(question: Question): Promise<void>
   save(question: Question): Promise<void>
-  findBySlug(slug: string): Promise<Question | null>
   delete(question: Question): Promise<void>
 }
