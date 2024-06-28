001b7eecacb2f0baedd92039a5e6fcb7e90889d0
Henrriky <tectutors.123@gmail.com>
2024-06-27 22:00:35 -0300

    feat: create abstract method findManyByTopicId to question comments repository

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,8 +1,13 @@
+import { PaginationParams } from "@/core/repositories/paginations-params";
 import { QuestionComment } from "../../enterprise/entities/Question-Comment";
 
 
 export interface QuestionCommentsRepository {
   findById(id: string): Promise<QuestionComment | null>
+  findManyByTopicId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<QuestionComment[]>
   create(questionComment: QuestionComment): Promise<void>
   delete(questionComment: QuestionComment): Promise<void>
 }
