ab6fd56fa7ff3ee230d085e51078d0108dc98a80
Henrriky <tectutors.123@gmail.com>
2024-06-27 23:29:07 -0300

    feat: create either pattern to error handling

---
+++ src/core/either.spec.ts
@@ -0,0 +1,29 @@
+import { Either, failure, success } from "./either"
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if(shouldSuccess) {
+    return success(10)
+  } else {
+    return failure('failure')
+  }
+}
+
+test('success result', () => {
+
+  const result = doSomething(true)
+
+  if (result.isSuccess()) {
+    console.log(result.value)
+  }
+
+  expect(result.isSuccess()).toBe(true)
+  expect(result.isFailure()).toBe(false)
+})
+
+test('failure result', () => {
+
+  const result = doSomething(false)
+
+  expect(result.isSuccess()).toBe(false)
+  expect(result.isFailure()).toBe(true)
+})
