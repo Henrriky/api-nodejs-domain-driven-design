9499da9fcf60416d882694afe184ccbf98aa4be6
Henrriky <tectutors.123@gmail.com>
2024-06-27 22:05:40 -0300

    feat: create abstract method findManyByAnswerId to answer comments repository

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,8 +1,13 @@
+import { PaginationParams } from "@/core/repositories/paginations-params";
 import { AnswerComment } from "../../enterprise/entities/Answer-Comment";
 
 
 export interface AnswerCommentsRepository {
   findById(id: string): Promise<AnswerComment | null>
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams
+  ): Promise<AnswerComment[]>
   create(answerComment: AnswerComment): Promise<void>
   delete(answerComment: AnswerComment): Promise<void>
 }
