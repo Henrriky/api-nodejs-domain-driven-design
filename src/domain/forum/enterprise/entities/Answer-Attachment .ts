6a9e9520c33259de3c16675243fee0bdc8d921fa
Henrriky <tectutors.123@gmail.com>
2024-06-30 21:04:19 -0300

    feat: create attachment domain entities

---
+++ src/domain/forum/enterprise/entities/Answer-Attachment .ts
@@ -0,0 +1,29 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+interface AnswerAttachmentProps {
+  answerId: UniqueEntityID
+  attachmentId: UniqueEntityID
+}
+
+
+export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
+  
+  public get answerId() {
+    return this.props.answerId
+  }
+
+  public get attachmentId() {
+    return this.props.answerId
+  }
+
+
+  static create(
+    props: AnswerAttachmentProps,
+    id?: UniqueEntityID,
+  ) {
+    const answerAttachment = new AnswerAttachment(props, id)
+
+    return answerAttachment
+  }
+}
---
+++ src/domain/forum/enterprise/entities/Attachment.ts
@@ -0,0 +1,18 @@
+import { Entity } from "@/core/entities/entity"
+
+
+export interface AttachmentProps {
+  title: string
+  link: string
+}
+
+export class Attachment extends Entity<AttachmentProps> {
+  
+  public get title() {
+    return this.props.title
+  }
+
+  public get link() {
+    return this.props.link
+  }
+}
