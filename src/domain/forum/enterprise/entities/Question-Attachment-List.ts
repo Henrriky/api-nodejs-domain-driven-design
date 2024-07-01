fa5444725b854a8cc307637ad97a65f963b7b6e0
Henrriky <tectutors.123@gmail.com>
2024-06-30 22:18:58 -0300

    feat: create question attachment list using watched list

---
+++ src/domain/forum/enterprise/entities/Question-Attachment-List.ts
@@ -0,0 +1,8 @@
+import { WatchedList } from "@/core/entities/watched-list";
+import { QuestionAttachment } from "./Question-Attachment";
+
+export class QuestionAttachmentList extends WatchedList<QuestionAttachment> {
+  compareItems(a: QuestionAttachment, b: QuestionAttachment): boolean {
+    return a.attachmentId === b.attachmentId
+  }
+}
